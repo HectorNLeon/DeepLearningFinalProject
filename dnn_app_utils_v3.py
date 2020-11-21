a338f39776b1f36c95df018a6f31316ad1e138cd
Hector Leon Quiroz <hectornleon98@gmail.com>
2020-11-21 16:20:16 -0700

    Added dev set to notebook

--- dnn_app_utils_v3.py
+++ dnn_app_utils_v3.py
@@ -90,14 +90,19 @@ def load_data():
     train_set_y_orig = np.array(dataset["train_labels"][:]) # your train set labels
 
     test_set_x_orig = np.array(dataset["test_img"][:]) # your test set features
-    test_set_y_orig = np.array(dataset["test_labels"][:]) # your test set labels
+    test_set_y_orig = np.array(dataset["test_labels"][:]) # your test set 
+    
+    dev_set_x_orig = np.array(dataset["dev_img"][:]) # your test set features
+    dev_set_y_orig = np.array(dataset["dev_labels"][:]) # your test set labels
+
 
     classes = np.array(dataset["list_classes"][:]) # the list of classes
     
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    dev_set_y_orig = dev_set_y_orig.reshape((1, dev_set_y_orig.shape[0]))
     
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, dev_set_x_orig, dev_set_y_orig, classes
 
 
 def initialize_parameters(n_x, n_h, n_y):
